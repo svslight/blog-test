94ee40800a0d5a12cbd8348284f680f89e8c1457
Svetlana Solodovnikova <svslight@yandex.ru>
2020-02-22 12:17:56 +0500

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+
+  # get 'static_pages/home'
+  root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
+
   resources :microposts
   resources :users
-  root 'users#index'
+  # root 'users#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
